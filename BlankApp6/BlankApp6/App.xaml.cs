9b177ea42ee827692f0ed83e26c8dd04dd904093
Leo <leomauv@gmail.com>
2022-06-30 23:02:34 +0200

    Ajoutez des fichiers projet.

---
+++ BlankApp6/BlankApp6/App.xaml.cs
@@ -0,0 +1,35 @@
+using BlankApp6.ViewModels;
+using BlankApp6.Views;
+using Prism;
+using Prism.Ioc;
+using Xamarin.Essentials.Implementation;
+using Xamarin.Essentials.Interfaces;
+using Xamarin.Forms;
+
+namespace BlankApp6
+{
+    public partial class App
+    {
+        public App(IPlatformInitializer initializer)
+            : base(initializer)
+        {
+        }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("/PrismMasterDetailPage/NavigationPage/MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterSingleton<IAppInfo, AppInfoImplementation>();
+
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+            containerRegistry.RegisterForNavigation<PrismMasterDetailPage, PrismMasterDetailPageViewModel>();
+            containerRegistry.RegisterForNavigation<ContentPageToto, ContentPageTotoViewModel>();
+        }
+    }
+}
---
+++ BlankApp6/BlankApp6/Views/PrismMasterDetailPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace BlankApp6.Views
+{
+    public partial class PrismMasterDetailPage : FlyoutPage
+    {
+        public PrismMasterDetailPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
